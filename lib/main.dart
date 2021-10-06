55017fc6e771b588845a6525ee8a042124e489fd
youse <dev@smartzone-it.com>
2021-10-06 09:30:02 +0200

    add accept and reject

--- lib/main.dart
+++ lib/main.dart
@@ -138,10 +138,3 @@ class Home extends StatelessWidget {
     );
   }
 }
-
-void _setupLogging() {
-  Logger.root.level = Level.ALL;
-  Logger.root.onRecord.listen((rec) {
-    print('${rec.level.name}: ${rec.time}: ${rec.message}');
-  });
-}
